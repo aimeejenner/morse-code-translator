1b61483f4cf0972d766d6ed4eeec3eda29300f47
aimeejenner <aimeejenner@gmail.com>
2022-03-22 21:33:32 +0000

    Added two extra tests to translator.test.js

--- dist/translator.test.dev.js
+++ dist/translator.test.dev.js
@@ -11,6 +11,10 @@ describe("Translate to Morse", function () {
     var output = new _translator.EnglishToMorse("I wandered lonely as a cloud");
     expect(output.joinWords()).toBe("../.-- .- -. -.. . .-. . -../.-.. --- -. . .-.. -.--/.- .../.-/-.-. .-.. --- ..- -..");
   });
+  test("Should not translate characters other than letters", function () {
+    var output = new _translator.EnglishToMorse("!£#3)9%*?");
+    expect(output.joinWords()).toBe("        ");
+  });
 });
 describe("Translate to English", function () {
   test("Should translate a single word from Morse to English", function () {
@@ -20,5 +24,9 @@ describe("Translate to English", function () {
   test("Should translate a phrase from Morse to English", function () {
     var output = new _translator.MorseToEnglish(".-/--. .. .- -. -/.-.. . .- .--./..-. --- .-./-- .- -. -.- .. -. -..");
     expect(output.joinWords()).toBe("A GIANT LEAP FOR MANKIND");
+  });
+  test("Should not translate characters other than dots or dashes", function () {
+    var output = new _translator.MorseToEnglish("aT,3c^2+!");
+    expect(output.joinWords()).toBe("");
   });
 });
--- translator.test.js
+++ translator.test.js
@@ -9,6 +9,10 @@ describe("Translate to Morse", () => {
         const output = new EnglishToMorse("I wandered lonely as a cloud");
         expect(output.joinWords()).toBe("../.-- .- -. -.. . .-. . -../.-.. --- -. . .-.. -.--/.- .../.-/-.-. .-.. --- ..- -..")
     });
+    test("Should not translate characters other than letters", () => {
+        const output = new EnglishToMorse("!£#3)9%*?");
+        expect(output.joinWords()).toBe("        ");
+    });
 })
 
 describe("Translate to English", () => {
@@ -19,5 +23,9 @@ describe("Translate to English", () => {
     test("Should translate a phrase from Morse to English", () => {
         const output = new MorseToEnglish(".-/--. .. .- -. -/.-.. . .- .--./..-. --- .-./-- .- -. -.- .. -. -..");
         expect(output.joinWords()).toBe("A GIANT LEAP FOR MANKIND")
+    });
+    test("Should not translate characters other than dots or dashes", () => {
+        const output = new MorseToEnglish("aT,3c^2+!");
+        expect(output.joinWords()).toBe("");
     });
 })
